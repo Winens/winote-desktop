a0b3886bc3757c638b052a348ebfdaef73589daa
Winens <2005ibrahimeldem@gmail.com>
2022-05-28 15:03:08 +0300

    base

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./assets/globals.css";
+import "@fontsource/plus-jakarta-sans/800.css";
+import "@fontsource/plus-jakarta-sans/700.css";
+import "@fontsource/plus-jakarta-sans/600.css";
+import "@fontsource/plus-jakarta-sans/500.css";
+import "@fontsource/plus-jakarta-sans/400.css";
+import "@fontsource/plus-jakarta-sans/300.css";
+
+import {createStore} from "vuex";
+
+const store = createStore({
+    state(){
+        return {
+            skip_welcome: localStorage.getItem("skip_welcome") === "__yes__"
+        }
+    }
+})
+
+createApp(App).use(store).mount("#app")
