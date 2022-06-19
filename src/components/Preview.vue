9cb679cf68e007e195498e1973860671e4a156e8
Winens <2005ibrahimeldem@gmail.com>
2022-06-19 22:17:12 +0300

    Fix: Overheight box size

--- src/components/Preview.vue
+++ src/components/Preview.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="previewer-box p-4 markdown-body flex border-l border-zinc-800">
+  <div class="previewer-box py-4 px-6 markdown-body flex border-l border-zinc-800">
     <VueMD :source="store.state.editor_model"/>
   </div>
 </template>
@@ -13,7 +13,8 @@
 <style>
   .previewer-box {
     position: relative;
-    height: 100vh;
+    height: calc(100vh - 24px);
+    font-family: Avenir, Helvetica, Arial, sans-serif;
   }
   .previewer-box h1 {
     font-size: 2.5em;
@@ -26,5 +27,8 @@
   .previewer-box h3 {
     font-size: 1.8em;
     font-weight: 600;
+  }
+  .previewer-box code {
+    font-size: 0.8em;
   }
 </style>
